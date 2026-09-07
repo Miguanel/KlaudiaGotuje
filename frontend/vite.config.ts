f2fe1afa029e9c5cd048147cc737cc5f6992ba6d
Migu <michauasota@gmail.com>
2026-09-07 11:56:06 +0200

    last usable v3.1.7

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// USUNIĘTE: import tailwindcss from ... (ponieważ nie jest używane w pliku konfiguracyjnym)
-
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Kluczowe dla poprawnego mapowania ścieżek przez Vite na produkcji
 })
