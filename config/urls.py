1ce77f892a7266376590a220b6c4a2a3c3607ef9
Migu <michauasota@gmail.com>
2026-09-07 11:54:46 +0200

    last usable v3.1.7

--- config/urls.py
+++ config/urls.py
@@ -1,27 +1,19 @@
 from django.contrib import admin
-from django.urls import path, re_path
-from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib.staticfiles.views import serve
+from django.urls import path
+from django.http import HttpResponse
 from recipes.api import api
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+def home_view(request):
+    return HttpResponse("Backend API działa poprawnie 💎")
+
 urlpatterns = [
+    path('', home_view, name='home'),
     path('admin/', admin.site.urls),
     path('api/', api.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # Bezpieczne serwowanie folderu assets bezpośrednio z STATIC_ROOT (staticfiles)
-    re_path(r'^assets/(?P<path>.*)$', serve, kwargs={'document_root': settings.STATIC_ROOT / 'assets'}),
-
-    # Wszystkie pozostałe adresy przejmuje React (Single Page Application)
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
