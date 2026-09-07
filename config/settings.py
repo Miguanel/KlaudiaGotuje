71c6e9ad95b4d02fa4de5d7b3d6b7b4aa4645627
Migu <michauasota@gmail.com>
2026-09-07 11:22:46 +0200

    last usable v3.1.5

--- config/settings.py
+++ config/settings.py
@@ -14,7 +14,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Użycie zmiennych z pliku .env
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'diamentowe-smaki-engibadwoman.onrender.com']
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'diamentowe-smaki-engibadwoman.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
